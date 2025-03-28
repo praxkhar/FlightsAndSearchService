4dcbba3609adb014aa0d6a111a8ef5bd60a19e24
Prakhar <prakharsaxena827@gmail.com>
2025-03-28 09:54:46 +0530

    Added airport seeders

--- README.md
+++ README.md
@@ -37,3 +37,8 @@ and then execute - `npx sequelize db:migrate`
 # City --> id,name, created_at, updated_at
 # Airport --> id, name, address, city_id, created_at, updated_at 
     Relationship -> City has many airports and airport belongs to a city (one to many)
+
+```
+npx sequelize model:generate --name Airport --attributes 
+name:String,address:String,,cityId:integer
+```
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/ServerConfig');
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index')
+// const { City, Airport } = require('./models/index');
+
 const SetupAndStartServer = async () => {   // there is no need of async fnc but later we will do DB connection or somthing else which might be async
     // create express object
     const app = express();
@@ -16,8 +19,12 @@ const SetupAndStartServer = async () => {   // there is no need of async fnc but
     app.use('/api', ApiRoutes)
 
     // start the server
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started on port ${PORT}`);
+        //syncing with environment
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({alert:true})
+        }
     })
 }
 
